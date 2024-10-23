e1e7cafe4d0d3bdb2857691604911ae5310287ff
johe01 <jhe0598@gmail.com>
2024-10-23 13:01:47 +0900

    rental, wish

---
+++ TJELibrary/src/main/java/tje/DTO/WishList.java
@@ -0,0 +1,19 @@
+package tje.DTO;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishList {
+
+	private int no;
+	private String id;
+	private int bookId;
+}
