17929e61008cd41e9299026bfb69cf97c196c147
koko2807 <0705ldh@hanmail.net>
2024-10-23 16:31:28 +0900

    Merge branch 'main' of https://github.com/xegenie/MiniProject1.git into main

---
+++ TJELibrary/src/main/java/tje/Service/BookServiceImpl.java
@@ -0,0 +1,95 @@
+package tje.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import tje.DAO.BookDAO;
+import tje.DAO.UserAuthDAO;
+import tje.DAO.UserDAO;
+import tje.DTO.Book;
+import tje.DTO.User;
+import tje.DTO.UserAuth;
+
+public class BookServiceImpl implements BookService{
+
+	UserDAO userDAO  = new UserDAO();
+	UserAuthDAO userAuthDAO  = new UserAuthDAO();
+	BookDAO bookDAO = new BookDAO();
+	
+	@Override
+	public Book select(int id) {
+		// 책 정보 조회
+		Book book = null;
+		try {
+			// pk 기준으로 조회
+			 book = bookDAO.select(id);
+			
+			// 조건으로 조회
+			// WHERE name = '김조은'
+			//   AND email = 'joeun@naver.com'
+//			Map<Object, Object> fields = new HashMap<Object, Object>() {{
+//	            put("name", "김조은");
+//	            put("email", "joeun@naver.com");
+//	        }};
+//			user = userDAO.selectBy(fields);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return book;
+	}
+	@Override
+	public int insert(Book book) {
+		int result = 0;
+		// 책 등록
+		try {
+			result = bookDAO.insert(book);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	@Override
+	public int delete(int id) {
+		// 책 삭제
+		int result = 0;
+		try {
+			result = userDAO.delete(id);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// 적용된 데이터 개수를 반환
+		// - result(결과)	: 0 --> 데이터 삭제 실패
+		//				  1 --> 삭제 수정 성공
+		if( result > 0 ) System.out.println("데이터 삭제 성공!");
+		else System.err.println("데이터 삭제 실패!");
+		return result;
+		
+	}
+	
+	@Override
+	public int update(Book book) {
+		// 책 수정
+		int result = 0;
+		try {
+			result = bookDAO.update(book);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// 적용된 데이터 개수를 반환
+		// - result(결과)	: 0 --> 데이터 수정 실패
+		//				      1 --> 데이터 수정 성공
+		if( result > 0 ) System.out.println("책 수정 성공!");
+		else System.err.println("책 수정 실패!");
+		return result;
+	}
+}
+
+
+		
+		
+		
--- TJELibrary/src/main/java/tje/Service/UserServiceImpl.java
+++ TJELibrary/src/main/java/tje/Service/UserServiceImpl.java
@@ -2,9 +2,9 @@ package tje.Service;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import tje.DAO.UserAuthDAO;
 import tje.DAO.UserDAO;
+import tje.DTO.RentalList;
 import tje.DTO.User;
 import tje.DTO.UserAuth;
 
@@ -16,7 +16,7 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public int signUp(User user) {
 		int result = 0;
-		// 회원 등록
+		// 회원 가입
 		try {
 			result = userDAO.insert(user);
 		} catch (Exception e) {
@@ -41,20 +41,20 @@ public class UserServiceImpl implements UserService{
 	
 	@Override
 	public User select(String id) {
-		
+		// 회원 조회
 		User user = null;
 		try {
 			// pk 기준으로 조회
-			// user = userDAO.select(id);
+			 user = userDAO.select(id);
 			
 			// 조건으로 조회
 			// WHERE name = '김조은'
 			//   AND email = 'joeun@naver.com'
-			Map<Object, Object> fields = new HashMap<Object, Object>() {{
-	            put("name", "김조은");
-	            put("email", "joeun@naver.com");
-	        }};
-			user = userDAO.selectBy(fields);
+//			Map<Object, Object> fields = new HashMap<Object, Object>() {{
+//	            put("name", "김조은");
+//	            put("email", "joeun@naver.com");
+//	        }};
+//			user = userDAO.selectBy(fields);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -62,7 +62,28 @@ public class UserServiceImpl implements UserService{
 		return user;
 	}
 
-	
+	@Override
+	public int delete(String id) {
+	// 회원 삭제
+	int result = 0;
+	try {
+		result = userDAO.delete(id);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	// 적용된 데이터 개수를 반환
+	// - result(결과)	: 0 --> 데이터 삭제 실패
+	//				  1 --> 삭제 수정 성공
+	if( result > 0 ) System.out.println("데이터 삭제 성공!");
+	else System.err.println("데이터 삭제 실패!");
+	return result;
+}
+	@Override
+	public RentalList select(int no) {
+		// 회원 대출 내역 조회
+		
+	}
 }
 
 
